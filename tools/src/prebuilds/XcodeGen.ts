1b43d38e641e9da7f153370585920e709221b3e3
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2021-11-03 14:50:23 +0100

    wip

--- tools/src/prebuilds/XcodeGen.ts
+++ tools/src/prebuilds/XcodeGen.ts
@@ -24,6 +24,7 @@ const PLATFORMS_MAPPING: Record<string, ProjectSpecPlatform> = {
 };
 
 export const INFO_PLIST_FILENAME = 'Info-generated.plist';
+export const GENERATED_MODULEMAP_FILENAME = 'generated.modulemap';
 
 /**
  * Generates `.xcodeproj` from given project spec and saves it at given dir.
@@ -127,6 +128,8 @@ export async function createSpecFromPodspecAsync(
         IPHONEOS_DEPLOYMENT_TARGET: podspec.platforms.ios,
         FRAMEWORK_SEARCH_PATHS: constructFrameworkSearchPaths(dependencies),
         HEADER_SEARCH_PATHS: constructHeaderSearchPaths(dependenciesNames),
+        SWIFT_IMPORT_PATHS: `$(inherited) /var/folders/qp/ympy822d7j97b4q14r7lx30w0000gn/T/Expo/DerivedData/Build/Products/Release-iphoneos/ExpoModulesCore.framework/Modules`,
+        MODULEMAP_FILE: podspec.modulemap_file ?? '',
 
         // Suppresses deprecation warnings coming from frameworks like OpenGLES.
         VALIDATE_WORKSPACE_SKIPPED_SDK_FRAMEWORKS: arrayize(podspec.frameworks).join(' '),
@@ -156,6 +159,7 @@ function constructHeaderSearchPaths(dependencies: string[]): string {
     ...dependencies,
 
     'DoubleConversion',
+    'ReactCommon',
     'React-callinvoker',
     'React-Core',
     'React-cxxreact',
@@ -208,7 +212,50 @@ function mergeXcodeConfigValue(prevValue: string | undefined, nextValue: string)
 function podNameToBundleId(podName: string): string {
   return podName
     .replace(/^UM/, 'unimodules')
-    .replace(/^EX/, 'expo')
+    .replace(/^(EX|Expo)/, 'expo')
     .replace(/(\_|[^\w\d\.])+/g, '.')
     .replace(/\.*([A-Z]+)/g, (_, p1) => `.${p1.toLowerCase()}`);
+}
+
+/**
+ * Generate custom modulemap for expo-modules-core which needs to make React-Core headers modular
+ */
+export async function generateExpoModulesCoreModulemapAsync(destDir: string): Promise<string> {
+  const expoModulesCoreUmbrellaHeaderName = 'ExpoModulesCore-umbrella.h';
+  const expoModulesCoreUmbrellaHeader = path.join(
+    PODS_PUBLIC_HEADERS_DIR,
+    'ExpoModulesCore',
+    expoModulesCoreUmbrellaHeaderName
+  );
+  await fs.copyFile(
+    expoModulesCoreUmbrellaHeader,
+    path.join(destDir, expoModulesCoreUmbrellaHeaderName)
+  );
+
+  const reactCoreHeaderDir = path.join(PODS_PUBLIC_HEADERS_DIR, 'React-Core');
+  const yogaUmbrellaHeader = path.join(PODS_PUBLIC_HEADERS_DIR, 'Yoga', 'Yoga-umbrella.h');
+
+  const modulemapContent = `
+ framework module ExpoModulesCore {
+   umbrella header "ExpoModulesCore.h"
+   export *
+   module * { export * }
+
+   module React {
+     umbrella "${reactCoreHeaderDir}"
+     export *
+     module * { export * }
+   }
+
+   module Yoga {
+     umbrella header "${yogaUmbrellaHeader}"
+     export *
+     module * { export * }
+   }
+ }
+ `;
+
+  const modulemapFile = path.join(destDir, GENERATED_MODULEMAP_FILENAME);
+  await fs.writeFile(modulemapFile, modulemapContent);
+  return modulemapFile;
 }
